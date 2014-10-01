61c08967e5407cf9e7c91bbe62fcf73842efc8d8
felixsockemartin <f.martin.1995@web.de>
2014-10-01 09:32:26 +0200

    Erster richtiger Commit

---
+++ Taschenrechner.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int a, b;
+float x, y;
+
+
+int zahleingabe(char x)
+{
+	if (x == '1')
+	{
+		puts("zahl 1 eingeben:");
+		scanf("%i", &a);
+		puts("zahl 2 eingeben:");
+		scanf("%i", &b);
+	}
+	else if (x == '2')
+	{
+		puts("zahl 1 eingeben:");
+		scanf("%i", &x);
+		puts("zahl 2 eingeben:");
+		scanf("%i", &y);
+	}
+	return 0;
+}
+
+
+int main()
+{
+	char befehl;
+	while (true)
+	{
+		puts("Was sollen sie tun?");
+		puts("a-addieren");
+		puts("s-subtrahieren");
+		puts("m-multiplizieren");
+		puts("d-dividieren");
+		puts("x-beenden");
+		scanf("%c", &befehl);
+		switch(befehl)
+		{
+			case 'a':
+				zahleingabe('1');
+				printf("Ihr Ergebnis: %i\n",a+b);
+			break;
+
+			case 's':
+				zahleingabe('1');
+				printf("Ihr Ergebnis: %i\n",a-b);
+			break;
+
+			case 'm':
+				zahleingabe('1');
+				printf("Ihr Ergebnis: %i\n",a*b);
+			break;
+
+			case 'd':
+				zahleingabe('2');
+				printf("Ihr Ergebnis: %f\n",x/y);
+			break;
+
+			case 'x':
+				return 0;
+			break;
+
+
+		}
+
+	}
+}
