3cd7a189f94a9c5637053109b81fe129746aeed2
felixsockemartin <dragon.felix@web.de>
2014-10-01 18:18:40 +0200

    Fertiger Rechner
    Es gibt noch einen kleinen Bug der aber bei der Benutzung nicht weiter
    stören sollte.

--- Taschenrechner.c
+++ Taschenrechner.c
@@ -2,70 +2,151 @@
 #include <stdbool.h>
 
 
-int a, b;
-float x, y;
-
-
-int zahleingabe(char x)
+int eingabe(int x)
 {
-	if (x == '1')
+	int zahl;
+	if (x == 0)
 	{
-		puts("zahl 1 eingeben:");
-		scanf("%i", &a);
-		puts("zahl 2 eingeben:");
-		scanf("%i", &b);
+		printf("Zahl eingeben:\n");
+		scanf("%i", &zahl);
 	}
-	else if (x == '2')
+	else
 	{
-		puts("zahl 1 eingeben:");
-		scanf("%f", &x);
-		puts("zahl 2 eingeben:");
-		scanf("%f", &y);
+		printf("Zahl %i eingeben:\n", x);
+		scanf("%i", &zahl);
 	}
-	return 0;
+	return zahl;
 }
 
 
 int main()
 {
-	char befehl;
+	int a, b, r, neu, alt, neuer;
+	float k, l;
 	while (true)
 	{
-		puts("Was sollen sie tun?");
-		puts("a-addieren");
-		puts("s-subtrahieren");
-		puts("m-multiplizieren");
-		puts("d-dividieren");
-		puts("x-beenden");
-		scanf("%c", &befehl);
-		switch(befehl)
+		puts ("Was willst du tun?");
+		puts ("+ - addieren");
+		puts ("- - subtrahieren");
+		puts ("* - multiplizieren");
+		puts ("/ - dividieren");
+		puts ("f - Fibonacci");
+		puts ("p - pythagoreisches Tripel");
+		puts ("w - Wechselwegnahme");
+		puts ("m - größter gemeinsamer Teiler mit Modulo");
+		puts ("x - beenden");
+		char befehl;
+		scanf ("%c", &befehl);
+		switch (befehl)
 		{
-			case 'a':
-				zahleingabe('1');
-				printf("Ihr Ergebnis: %i\n",a+b);
+			case '+':
+				a = eingabe(1);
+				b = eingabe(2);
+				printf("Ihr Ergebnis: %i\n", a + b);
 			break;
-
-			case 's':
-				zahleingabe('1');
-				printf("Ihr Ergebnis: %i\n",a-b);
+			case '-':
+				a = eingabe(1);
+				b = eingabe(2);
+				printf("Ihr Ergebnis: %i\n", a - b);
 			break;
-
+			case '*':
+				a = eingabe(1);
+				b = eingabe(2);
+				printf("Ihr Ergebnis: %i\n", a * b);
+			break;
+			case '/':
+				puts("Zahl 1 eingeben:");
+				scanf("%f", &k);
+				puts("Zahl 2 eingeben:");
+				scanf("%f", &l);
+				if (b == 0)
+				{
+					printf("Teilen durch 0 ist nicht Möglich");
+				}
+				else
+				{
+					printf("Ihr Ergebnis: %f\n", k/l);
+				}
+			break;
+			case 'f':
+				neu = 1;
+				alt = 0;
+				a = eingabe(0);
+				for (int i = 1; i <= a; i++)
+				{
+					neuer = alt + neu;
+					alt = neu;
+					neu = neuer;
+				}
+				printf("Ergebnis: %i\n", neuer);
+			break;
+			case 'p':	
+				a = eingabe(0);
+				for(int x = 1; x <= a; x++)
+				{
+					for(int y = x + 1; y <= a; y++)
+					{
+						if (x + y <= a)
+						{
+							for (int z = y + 1; z <= a; z++)
+							{
+								if ( x + y + z <= a)
+								{
+									if (a == x + y + z && z * z == (y * y) + (x * x))
+									{
+										printf("Ein/Das Triplet ist: x = %i y = %i z = %i\n", x, y, z);
+									}
+								}
+								else
+								{
+									z = a;
+								}
+							}
+						}
+						else
+						{
+							y = a;
+						}
+					}
+				}
+			break;
+			case 'w':
+				a = eingabe(1);
+				b = eingabe(2);
+				while (a != b)
+				{
+					if (a > b)
+					{
+						a -= b;
+					}
+					else
+					{
+						b -= a;
+					}
+				}
+				printf("Ergebnis: %i\n", a);
+			break;
 			case 'm':
-				zahleingabe('1');
-				printf("Ihr Ergebnis: %i\n",a*b);
+				a = eingabe(1);
+				b = eingabe(2);
+				if (a != 0 || b != 0)
+				{
+					while (b != 0)
+					{
+						r = a % b;
+						a = b;
+						b = r;
+					}
+					printf("Ergebnis: %i\n", a);
+				}
+				else
+				{
+					printf("Keine Der Zahlen dürfen 0 sein!");
+				}
 			break;
-
-			case 'd':
-				zahleingabe('2');
-				printf("Ihr Ergebnis: %f\n",x/y);
-			break;
-
 			case 'x':
 				return 0;
 			break;
-
-
 		}
-
 	}
 }
