91bd9b5c3c8001f338353b6813b8af9f5a75e92c
felixsockemartin <dragon.felix@web.de>
2014-10-08 21:03:43 +0200

    Multiplikationen und Primzahlen hinzugefügt

--- Taschenrechner.c
+++ Taschenrechner.c
@@ -1,9 +1,122 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int pot(int zahl, int potenz)
+{
+	if (potenz == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		int tmp = 1;
+		for (potenz; potenz > 0; potenz--)
+		{
+			tmp = mult(tmp, zahl);
+		}
+		return tmp;
+	}
+	
+}
 
+int agypt(int x, int y)
+{
+	int E = 0;
+	while (x > 1)
+	{
+		if(x % 2 != 0)
+		{
+			E = E + y;
+		}
+		y = y + y;
+		x = x/2;
+	}
+	return E;
+}
 
-int Multiplikation_mit_Summierung(int x, int y)
+int mult(int zahl1, int zahl2)
+{
+	int E = 0;
+	for (int i = 1; i <= zahl2; i++)
+	{
+		E += zahl1;
+	}
+	return E;
+}
+
+int length(int value)
+{
+	int l=1;
+	while(value > 9)
+	{
+		l++; value/=10;
+	}
+	return l;
+}
+
+int chinmulti(int x, int y)
+{
+	int E = 0, ymerkwert = y; s, r, lx, ly;
+	lx = length(x);
+	ly = length(y);
+	for(lx; lx >= 0; lx--)
+	{
+		s = x / pot(10, lx);
+		x = x - mult(s, pot(10, lx));
+		y = ymerkwert;
+		ly = length(y);
+		for(ly; ly >= 0; ly--)
+		{
+			r = y / pot(10, ly);
+			y = y - mult(r, pot(10, ly));
+			E = E + mult(mult(s, pot(10, lx)), mult(r, pot(10, ly)));
+		}
+	}
+	return E;
+}
+
+int binfak(int x int y)
+{
+	int E = 0, n = 0;
+	while(pot(2, n) < x)
+	{
+		n++;
+	}
+	for(n; n >= 0; n--)
+	{
+		if(x - pot(2, n) > 0)
+		{
+			E = E + mult(pot(2, n) > 0);
+			x = x - pot(2, n);
+		}
+	}
+	return E;
+}
+
+void prim(int x)
+{
+	int g = 0 i = 3;
+	bool test;
+	while(g < x)
+	{
+		test = true;
+		for(int k = i - 1; k > 2; k--)
+		{
+			if(i % k == 0)
+			{
+				test = false;
+			}
+		}
+		if(test)
+		{
+			g++;
+			printf("Primzahl Nummer %i lautet: %i", g, i);
+		}
+		i++;
+	}
+}
+
+/*int Multiplikation_mit_Summierung(int x, int y)
 {
 	int E = 0;
 	for(int i = 1; i < x; i++)
@@ -26,7 +139,7 @@ int Potenz(int x, int y)
 int chinesisch_Multiplizieren(int x, int y)
 {
 	int lx;
-}
+}*/
 
 int eingabe(int x)
 {
@@ -58,12 +171,13 @@ int main()
 		puts ("/ - dividieren");
 		puts ("f - Fibonacci");
 		puts ("p - pythagoreisches Tripel");
+		puts ("r - Primzahlen");
 		puts ("w - Wechselwegnahme");
 		puts ("m - größter gemeinsamer Teiler mit Modulo");
 		puts ("x - beenden");
-		char befehl;
-		scanf ("%c", &befehl);
-		switch (befehl)
+		char befehl[5];
+		scanf ("%s", &befehl);
+		switch (befehl[0])
 		{
 			case '+':
 				a = eingabe(1);
@@ -78,7 +192,30 @@ int main()
 			case '*':
 				a = eingabe(1);
 				b = eingabe(2);
-				printf("Ihr Ergebnis: %i\n", a * b);
+				puts("Wie wollen sie Multiplizieren?");
+				puts("b - Binärische Methode");
+				puts("a - AEgyptische Methode");
+				puts("c - Chinesische Methode");
+				puts("n - normales Multiplizieren (immer wieder addieren)");
+				scanf("%s", &befehl);
+				switch(befehl[0])
+				{
+					case 'b':
+						puts("Ihr Ergebnis: %i", binfak(a, b))
+					break;
+					case 'a':
+						puts("Ihr Ergebnis: %i", agypt(a, b))
+					break;
+					case 'c':
+						puts("Ihr Ergebnis: %i", chinmulti(a, b))
+					break;
+					case 'n':
+						puts("Ihr Ergebnis: %i", mult(a, b))
+					break;
+					default:
+					puts("falsche Eingabe");
+					break;
+				}
 			break;
 			case '/':
 				puts("Zahl 1 eingeben:");
@@ -136,6 +273,10 @@ int main()
 					}
 				}
 			break;
+			case 'r'
+				a = eingabe(0);
+				prim(a);
+			break;
 			case 'w':
 				a = eingabe(1);
 				b = eingabe(2);
@@ -173,7 +314,9 @@ int main()
 			case 'x':
 				return 0;
 			break;
+			default:
+				puts("falsche Eingabe");
+			break;
 		}
-		fflush(stdout);
 	}
 }
